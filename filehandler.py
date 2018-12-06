1bbad0af0788e8a784c68486f8e80eba51c8c94c
Jessica Sharp <31940586+jesharp001@users.noreply.github.com>
2018-12-06 15:23:00 -0600

    Update filehandler.py

--- filehandler.py
+++ filehandler.py
@@ -1,14 +1,40 @@
 """
 Version 1: raw row with directory paths and file names
-in one row. 
+in one row.
+
+Python 3.7.1
 """
 import os
 
 # set the master parent directory
-path = input("Where is your top most parent directory? > ")
+# uncomment the line below in final draft
+# path = input("Where is your top most parent directory? > ")
+#
+# hard code test directory below for testing convenience
+path = 'c:/pythontest'
+
+"""
 
 # print a row with directory paths and files inside
 for file in os.walk(path):
     for filename in file:
         print(file)
+
+# visual line to separate test 1 from test 2)
+print('-----------------------------------------------------------\n')
+"""
+
+# print each file/doc on a row with full path
+for subdir, dirs, files in os.walk(path):
+    for file in files:
+        if file.endswith('.zip'):
+            pass
+        else:
+            print(os.path.join(subdir, file))
+
+for subdir, dirs, files in os.walk(path):
+    for file in files:
+
+        #print(os.path.isdir(file)) #returns False because class = str
+        print(type(file))
 
